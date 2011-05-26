b1f680bb102c8b9f430154e2bc7c4143cab8801e
rwldrn <waldron.rick@gmail.com>
2011-05-26 10:29:32 -0400

    Upgrades jQuery.Topic to be passthrough to Topic ctor creating real instances

--- src/topic.js
+++ src/topic.js
@@ -1,43 +1,69 @@
 (function( jQuery ) {
 
-	var topics = {},
-		sliceTopic = [].slice;
+	var sliceTopic = [].slice;
 
-	jQuery.Topic = function( id ) {
-		var callbacks,
-			method,
-			topic = id && topics[ id ];
-		if ( !topic ) {
-			callbacks = jQuery.Callbacks();
-			topic = {
-				publish: callbacks.fire,
-				subscribe: callbacks.add,
-				unsubscribe: callbacks.remove
-			};
-			if ( id ) {
-				topics[ id ] = topic;
-			}
+	jQuery.Topic = function( name ) {
+
+		// Localize refs for compression benefit
+		var topicCache = jQuery.Topic.cache, 
+			fromCache = topicCache[ name ];
+
+		// If topic object exists, return it
+		if ( fromCache ) {
+			return fromCache;
 		}
-		return topic;
+
+		// Create new topic object instance
+		return new Topic( name );
 	};
 
+	jQuery.Topic.cache = {};
+	jQuery.Topic.inherits = {
+		"publish": "fire",
+		"subscribe": "add",
+		"unsubscribe": "remove"
+	};
+
+	function Topic( name ) {
+
+		// Localize  refs for compression benefit
+		var topicCache = jQuery.Topic.cache,
+			inherits = jQuery.Topic.inherits,
+			callbacks = jQuery.Callbacks(),
+			selfObj = {};
+
+		this.name = name;
+
+		jQuery.each( inherits, function( method, inherit ) {
+			selfObj[ method ] = callbacks[ inherit ];
+		});
+
+		jQuery.extend( this, selfObj );
+
+		topicCache[ name ] = this;
+
+		return this;
+	}
+
 	jQuery.extend({
-		subscribe: function( id ) {
-			var topic = jQuery.Topic( id ),
+		subscribe: function( name /*, callback */ ) {
+			var topic = jQuery.Topic( name ),
 				args = sliceTopic.call( arguments, 1 );
+
 			topic.subscribe.apply( topic, args );
+
 			return {
 				topic: topic,
 				args: args
 			};
 		},
-		unsubscribe: function( id ) {
-			var topic = id && id.topic || jQuery.Topic( id );
-			topic.unsubscribe.apply( topic, id && id.args ||
+		unsubscribe: function( name/*, callback */ ) {
+			var topic = name && name.topic || jQuery.Topic( name );
+			topic.unsubscribe.apply( topic, name && name.args ||
 					sliceTopic.call( arguments, 1 ) );
 		},
-		publish: function( id ) {
-			var topic = jQuery.Topic( id );
+		publish: function( name ) {
+			var topic = jQuery.Topic( name );
 			topic.publish.apply( topic, sliceTopic.call( arguments, 1 ) );
 		}
 	});
